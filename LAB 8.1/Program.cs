12bdb6e357be9d646c36472d0ea3f4033ff48285
Sergey <Sergey@DESKTOP-PQ9HUE6>
2021-12-19 15:24:41 +0300

    Добавьте файлы проекта.

---
+++ LAB 8.1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LAB_8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirName= (@"D:\объекты");
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Подкаталоги");
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Файлы");
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+            }
+
+
+
+
+
+            DirectoryInfo d = new DirectoryInfo(@"D:\объекты");
+            Console.WriteLine("Список каталогов");
+            foreach (var item in d.GetDirectories())
+                {
+                Console.WriteLine(item.Name);
+                Console.WriteLine("список подкаталогов");
+                foreach (var it in item.GetDirectories())
+                    Console.WriteLine(item.Name);
+                Console.WriteLine();
+                }
+            
+            
+            Console.ReadLine();
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ LAB 8.2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace LAB_8._2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            string path = "Logs2/log.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+
+            using (StreamWriter sw = new StreamWriter(path, true));
+            {
+
+                int[] array = new int[10];
+                Random random = new Random();
+                int S = 0;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    array[i] = random.Next(0, 10);
+                    S += array[i];
+                    Console.Write("{0}", array[i]);
+                }
+                Console.WriteLine();
+                Console.WriteLine(S);
+
+
+
+            }
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
